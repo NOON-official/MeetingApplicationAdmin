9a3fe01a2c7bdfc9369022a6a2b074532041ddc4
Darron Park <darron1217@gmail.com>
2023-02-17 18:42:00 +0900

    feat: 신청완료 목록 구현중

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,12 +1,42 @@
 import Section from "@/components/Section";
 import BaseLayout from "@/layouts/BaseLayout";
+import styled from "styled-components";
 
-const IndexPage = () => (
-  <BaseLayout>
-    <Section center my="32px">
-      <h2>관리자 페이지</h2>
-    </Section>
-  </BaseLayout>
-);
+const IndexPage = () => {
+  return (
+    <BaseLayout>
+      <Section center my="32px">
+        <h2>관리자 페이지</h2>
+        <StatTable>
+          <tbody>
+            <tr>
+              <td>3:3</td>
+              <td>남 : 55</td>
+              <td>여 : 55</td>
+              <td>전체 : 110</td>
+            </tr>
+            <tr>
+              <td>2:2</td>
+              <td>남 : 55</td>
+              <td>여 : 55</td>
+              <td>전체 : 110</td>
+            </tr>
+          </tbody>
+        </StatTable>
+      </Section>
+    </BaseLayout>
+  );
+};
+
+const StatTable = styled.table`
+  display: inline-block;
+
+  td {
+    width: 140px;
+    height: 40px;
+    border: 1px solid #000000;
+    font-weight: bold;
+  }
+`;
 
 export default IndexPage;

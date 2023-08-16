b15f021bf9c0c8505d021971034a4a88f4495e7a
kimboyoon <kimboyoon0908@gmail.com>
2023-08-16 18:36:29 +0900

    chore: studentcard get api and mapping

--- src/pages/studentcard/index.page.tsx
+++ src/pages/studentcard/index.page.tsx
@@ -1,35 +1,34 @@
-import Section from "@/components/Section";
-import { useGetAdminUserStudentCardQuery } from "@/features/user/api";
-import LayoutWithHeader from "@/layouts/LayoutWithHeader";
-import { Button } from "antd";
-import { useState } from "react";
-import styled from "styled-components";
+import Section from '@/components/Section';
+import { useGetAdminUserStudentCardQuery } from '@/features/user/api';
+import LayoutWithHeader from '@/layouts/LayoutWithHeader';
+import { Button } from 'antd';
+import styled from 'styled-components';
 
 const StudentcardPage = () => {
-  const [url, setUrl] = useState<string>(``);
-  const aa = useGetAdminUserStudentCardQuery();
-  console.log(aa);
+  const { data: users } = useGetAdminUserStudentCardQuery();
 
   return (
     <LayoutWithHeader>
-      <Section center my="32px" display="flex">
-        <ImgContainer>
-          <Img
-            src="https://studentidcard.s3.ap-northeast-2.amazonaws.com/%EA%B9%80%EB%B3%B4%EC%9C%A4LKH3TZ84.jpg"
-            alt="studentcardimg"
-          />
-          <DetailContainer>
-            <Info>이름 : 김보윤</Info>
-            <Info>성별 : 여자</Info>
-            <Info>학교 : 한국외국어대학교</Info>
-            <Info>나이 : 26</Info>
-          </DetailContainer>
-        </ImgContainer>
-        <ButtonBox>
-          <Btn>인증 완료</Btn>
-          <Btn>인증 실패</Btn>
-        </ButtonBox>
-      </Section>
+      {users?.users.map((user) => {
+        return (
+          <Section center my="32px" display="flex" key={user.userId}>
+            <ImgContainer>
+              <Img src={user.studentCardUrl} alt="studentcardimg" />
+
+              <DetailContainer>
+                <Info>{`이름 : ${user.nickname}`}</Info>
+                <Info>{`성별 : ${user.gender}`}</Info>
+                <Info>{`학교 : ${user.university}`}</Info>
+                <Info>{`나이 : ${user.birth}`}</Info>
+              </DetailContainer>
+            </ImgContainer>
+            <ButtonBox>
+              <Btn>인증 완료</Btn>
+              <Btn>인증 실패</Btn>
+            </ButtonBox>
+          </Section>
+        );
+      })}
     </LayoutWithHeader>
   );
 };
@@ -44,7 +43,7 @@ const ImgContainer = styled.div`
 `;
 
 const Img = styled.img`
-  height: 400px;
+  height: 300px;
 `;
 
 const DetailContainer = styled.div`
